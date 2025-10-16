1bdae96c57d889fe5b2dc8f3abec12679260c13c
Abdullah Al Maruf <moshiarrahman050@gmail.com>
2025-10-16 23:43:35 +0600

    fixed cors stuff

--- Bit/Bit/settings.py
+++ Bit/Bit/settings.py
@@ -148,8 +148,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '<your-google-client-id>'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '<your-google-client-secret>'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://hoppscotch.io",
-    "http://localhost:3000",
+    "https://bit-front-end.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
